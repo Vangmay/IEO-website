cb84e464b28e0a3c553c90fe1e82b613ab1bfa7d
Vangmay Sachan <vincetheutuber@gmail.com>
2021-10-27 19:55:11 +0530

    Added earth

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 const hamburger = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector(".mobile-nav");
+const hero = document.querySelector('.hero');
+
 hamburger.addEventListener('click',function (){
     this.classList.toggle('is-active')
     mobile_menu.classList.toggle('is-active');
 })
 
-//THREEEEE..
-var scene, camera,renderer;
+//Threee
+const Width = hero.clientWidth
+const Height=  hero.clientHeight
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdddddd);
 
-function init(){
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xdddddd);
-    width = window.innerHeight
-    Height = window.innerWidth
-    console.log(width)
-    camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
-    camera.rotation.y = 45/180 * Math.PI
-    camera.position.x = 1800;
-    camera.position.y = 100;
-    camera.position.z = 1200;
+const camera = new THREE.PerspectiveCamera(40,Width/Height,1,5000);
+camera.rotation.y = 45/180 * Math.PI
+camera.position.x = 1600;
+camera.position.y = 100;
+camera.position.z = 1300;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( Width,Height );
+hero.appendChild( renderer.domElement );
+
+const hlight = new THREE.AmbientLight(0x404040,3);
+scene.add(hlight)
+
+
+window.addEventListener( 'resize', onWindowResize );
+
+
+let loader = new THREE.GLTFLoader();
+loader.load('./src/Earth.gltf',function(gltf){
+    earth = gltf.scene.children[0]
+    scene.scale.set(1,1,1)
+    scene.add(earth);
+})
+
+function animate() {
+    requestAnimationFrame( animate );
+	renderer.render( scene, camera );
     
-
-    hlight = new THREE.AmbientLight(0x404040,3);
-    scene.add(hlight)
-
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement)
-
-    function onResize(){
-        var Width = window.innerWidth;
-        var Height = window.innerHeight;
-        renderer.setSize(Width,Height)
-        camera.aspect = Width/Height;
-        camera.updateProjectionMatrix()
-
-        renderer.setSize(Width,Height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    }
-    window.addEventListener('resize',onResize)
-
-    let loader = new THREE.GLTFLoader();
-    loader.load('./src/Earth.gltf',function(gltf){
-        earth = gltf.scene.children[0]
-        scene.add(earth);
-        renderer.render(scene,camera);
-    })
 }
+animate();
 
-init(); 
-//THREEEEE..
-        
+
+function onWindowResize() {
+    let Width = hero.clientWidth
+    let Height = hero.clientHeight
+    camera.aspect = Width/ Height;
     
-   
-
+    camera.updateProjectionMatrix();
+    renderer.setSize( Width,Height );
+    
+}
+//Threee
