9f6f59f57908bb2cda44edc1cd96d7989bd720a3
Ojas <ojas1807@gmial.com>
2021-11-08 13:17:38 +0530

    Colour changes

--- Help/script.js
+++ Help/script.js
@@ -6,3 +6,7 @@ hamburger.addEventListener('click',function (){
     this.classList.toggle('is-active')
     mobile_menu.classList.toggle('is-active');
 })
+
+$(document).ready(function(){
+    $(".hero").ripples({resolution: 256, perturbance: 0.1});
+  })
